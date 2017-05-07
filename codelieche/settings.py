e5124fc4b7082eadf229f61d41b2729e8577961e
codelieche <codelieche@gmail.com>
2017-05-07 09:15:05 +0800

    把app统一放apps目录中

--- codelieche/settings.py
+++ codelieche/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# 把apps添加到path路径中，app🙆放apps中
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
