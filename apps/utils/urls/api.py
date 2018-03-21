7c27b5ef52ee3ce435a4a2d69a098f36d6b618ee
codelieche <codelieche@gmail.com>
2018-03-21 21:44:52 +0800

    导出Model数据api

--- apps/utils/urls/api.py
+++ apps/utils/urls/api.py
@@ -1,10 +1,12 @@
 # -*- coding:utf-8 -*-
 from django.conf.urls import url
 
-from utils.views.export import TestExportDataView
+from utils.views.export import ExportDataView, TestExportDataView
 
 urlpatterns = [
     # api url前缀：/api/v1/utils/
-
+    # 导出数据
+    url(r'^export/data/?$', ExportDataView.as_view(), name="export_data"),
     url(r'^export/test/?$', TestExportDataView.as_view(), name="export_test"),
+
 ]
