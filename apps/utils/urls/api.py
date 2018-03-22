b793da574f86edafb1099e38fe6f1b2886418f94
codelieche <codelieche@gmail.com>
2018-03-22 08:40:51 +0800

    Model导入接口

--- apps/utils/urls/api.py
+++ apps/utils/urls/api.py
@@ -2,11 +2,15 @@
 from django.conf.urls import url
 
 from utils.views.export import ExportDataView, TestExportDataView
+from utils.views.imports import ImportDataToModelView
 
 urlpatterns = [
     # api url前缀：/api/v1/utils/
     # 导出数据
     url(r'^export/data/?$', ExportDataView.as_view(), name="export_data"),
     url(r'^export/test/?$', TestExportDataView.as_view(), name="export_test"),
+
+    # 导入数据
+    url(r'^import/data', ImportDataToModelView.as_view(), name="import_data"),
 
 ]
